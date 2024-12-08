c77f9be97be29673dd9be8648300689ee0a64466
viniciuspascucci1993 <vinicius.pascucci1@gmail.com>
2024-12-08 18:54:52 -0300

    Projeto backend criado

---
+++ src/main/java/com/calcados/store/CalcadosStoreApplication.java
@@ -0,0 +1,13 @@
+package com.calcados.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcadosStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcadosStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calcados/store/CalcadosStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calcados.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcadosStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
